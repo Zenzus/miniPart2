09f1bd7499218756672e42db924953746dacd2b5
Zenzus <jonas.hein25@gmail.com>
2019-04-21 13:11:07 +0200

    first

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
-const DEV_DB_URI = "mongodb+srv://BossMan:12345@adjlyngby-9djsz.mongodb.net/Mini_Production?retryWrites=true";
-const TEST_DB_URI = "mongodb+srv://BossMan:12345@adjlyngby-9djsz.mongodb.net/Mini_test?retryWrites=true";
+const DEV_DB_URI = "mongodb+srv://jonas:koko123@cluster0-utqvq.azure.mongodb.net/miniProDevDb?retryWrites=true";
+const TEST_DB_URI = "mongodb+srv://jonas:koko123@cluster0-utqvq.azure.mongodb.net/miniProTestDb?retryWrites=true";
+
 const MOCHA_TEST_TIMEOUT = 5000;
 
 module.exports = {
